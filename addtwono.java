447b2aba730b00de1563154e7c6e79806082cf18
Nanditasaravanan07 <144513305+nandhitasaravanan0701@users.noreply.github.com>
2026-03-03 09:58:18 +0530

    Add files via upload

---
+++ addtwono.java
@@ -0,0 +1,26 @@
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummyHead = new ListNode(0);
+        ListNode current = dummyHead;
+        int carry = 0;
+
+        while (l1 != null || l2 != null || carry != 0) {
+            int val1 = (l1 != null) ? l1.val : 0;
+            int val2 = (l2 != null) ? l2.val : 0;
+
+            // Calculate sum and new carry
+            int sum = val1 + val2 + carry;
+            carry = sum / 10;
+            
+            // Create new node with the digit portion of the sum
+            current.next = new ListNode(sum % 10);
+            
+            // Move pointers forward
+            current = current.next;
+            if (l1 != null) l1 = l1.next;
+            if (l2 != null) l2 = l2.next;
+        }
+
+        return dummyHead.next;
+    }
+}
